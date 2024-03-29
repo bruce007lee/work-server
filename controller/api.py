ae270b1768832712bdc4efe194528bbb7288f1c1
bruce <bruce@example.com>
2024-03-29 10:33:24 +0000

    feat: init

--- controller/api.py
+++ controller/api.py
@@ -2,19 +2,22 @@ from flask import Blueprint, request
 from PIL import Image
 import numpy as np
 from service import ocr as ocrService
+from utils.logger import logger
 
 app = Blueprint("api", __name__)
 
-"""
-文字识别接口
-"""
+
 @app.route("/ocr", methods=["POST"])
 def ocr():
+    """
+    文字识别接口
+    """
     try:
         file = request.files["file"]
+        lang = request.form["lang"]
         image = Image.open(file)
-        data = ocrService.recognize(np.array(image))
+        data = ocrService.recognize(img=np.array(image), lang=lang)
         return {"success": True, "data": data}
     except:
-        app.logger.error('Recognize error [%s]', file.filename)
+        logger.error("Recognize error [%s]", file.filename)
         return {"success": False, "errorMessage": "Recognize error"}
--- service/ocr.py
+++ service/ocr.py
@@ -1,7 +1,8 @@
 from paddleocr import PaddleOCR
 import json
 
-def recognize(img):
-    ocr = PaddleOCR(use_angle_cls=True, lang="ch", use_gpu=True, show_log=False)
+
+def recognize(img, lang="ch"):
+    ocr = PaddleOCR(use_angle_cls=True, lang=lang, use_gpu=True, show_log=False)
     result = ocr.ocr(img, cls=True)
     return json.dumps(result)
