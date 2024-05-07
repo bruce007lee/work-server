a6f08a1d9ddccf6aee21b6b8304f0e00c3a2c9f8
lizhen <lizhen@example.com>
2024-05-07 07:16:34 +0000

    chore: up script

--- stop.sh
+++ stop.sh
@@ -13,9 +13,9 @@ if [ ! -z "$PID" ]; then
     
     # 检查进程是否被成功杀死
     if sudo kill -0 $PID > /dev/null 2>&1; then
-        echo "进程 $PID 未能被杀死。"
-    else
         echo "进程 $PID 已被杀死。"
+    else
+        echo "进程 $PID 未能被杀死。"
     fi
 else
     echo "没有发现占用端口 $PORT 的进程。"
