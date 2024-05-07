d988ce6c44a03efde8ad5740c127fbdfb1b3067b
lizhen <lizhen@example.com>
2024-05-07 09:07:45 +0000

    feat: adjust options

--- service/ocr.py
+++ service/ocr.py
@@ -1,5 +1,5 @@
 from paddleocr import PaddleOCR
 
 def recognize(img, lang="ch"):
-    ocr = PaddleOCR(use_angle_cls=True, lang=lang, use_gpu=True, show_log=False, det_db_unclip_ratio=2.1)
+    ocr = PaddleOCR(use_angle_cls=True, lang=lang, use_gpu=True, show_log=False)
     return ocr.ocr(img, cls=True)
